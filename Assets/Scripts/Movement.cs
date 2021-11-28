f2a8f2981d67068fbdca16078714a0acab778fe8
IbrahimHamwi <ibrahimhamwi_619@hotmail.com>
2021-11-28 18:59:25 +0300

    Added Rocket shape and particle effect

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 1000f;
-    [SerializeField]
-    float rotationThrust = 100f;
+    [SerializeField] float rotationThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
     Rigidbody rb;
     AudioSource audioSource;
     // Start is called before the first frame update
@@ -29,7 +29,7 @@ public class Movement : MonoBehaviour
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
             if (!audioSource.isPlaying)
             {
-                audioSource.Play();
+                audioSource.PlayOneShot(mainEngine);
             }
         }
         else
